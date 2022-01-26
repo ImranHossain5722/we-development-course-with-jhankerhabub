e04b6fc0644b565366d1af4cdc44b4a1512ebba0
imranH <imd572258@gmail.com>
2022-01-26 07:00:29 +0600

    all folder save

---
+++ 4th-Maliston-JavaScript/inro-js/first.js
@@ -0,0 +1,14 @@
+//  string variable 
+var name = imran;
+// numeric variable 
+var age = 25;
+// variable boolean
+var ans = true;
+var ans = false;
+
+//Simple Mathematical operations in javascript
+
+var aggPrice = 50 ;
+var penPrice = 54;
+console.log(aggPrice);
+
